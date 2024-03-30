ee6319f4ec71afbca3f4c3a98bcc5c7849194475
Harry-Kachigamba <152857607+Harry-Kachigamba@users.noreply.github.com>
2024-03-31 01:51:01 +0200

    Update index.js

--- src/lib/index.js
+++ src/lib/index.js
@@ -3,6 +3,7 @@ import { Client, Account } from 'appwrite';
 
 export const client = new Client();
 
+//Best practice to place projectID & API Endpoint in .env file
 client
     .setEndpoint('<API-Endpoint>')
     .setProject('<projectID>'); // Replace with your project ID
